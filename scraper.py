84221daf70bb7c6efefa982a1cad0970b3a03f41
cvrabel <cjv38@cornell.edu>
2016-09-24 11:38:00 -0400

    data to csv, only 2 players

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import os 
-
+import pandas as pd
 u_a = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36"
 
 # seasons of interest 
@@ -16,17 +16,29 @@ for season in seasons:
   player_data = response.json()['resultSets'][0]['rowSet']
   season_to_player_ids[season] = [x[0] for x in player_data]
 
-print season_to_player_ids
 
 shot_url = 'http://stats.nba.com/stats/shotchartdetail?Period=0&VsConference=&LeagueID=00&LastNGames=0&TeamID=0&Position=&Location=&ContextMeasure=FGA&DateFrom=&StartPeriod=&DateTo=&OpponentTeamID=0&ContextFilter=&RangeType=&Season=%s&AheadBehind=&EndRange=&VsDivision=&PointDiff=&RookieYear=&GameSegment=&Month=0&ClutchTime=&StartRange=&EndPeriod=&SeasonType=Regular+Season&SeasonSegment=&GameID=&PlayerID=%s&Outcome='
 
+headings = ["GRID_TYPE","GAME_ID","GAME_EVENT_ID", "PLAYER_ID","PLAYER_NAME",
+  "TEAM_ID","TEAM_NAME","PERIOD","MINUTES_REMAINING","SECONDS_REMAINING",
+  "EVENT_TYPE","ACTION_TYPE","SHOT_TYPE","SHOT_ZONE_BASIC", "SHOT_ZONE_AREA",
+  "SHOT_ZONE_RANGE","SHOT_DISTANCE","LOC_X","LOC_Y",
+  "SHOT_ATTEMPTED_FLAG","SHOT_MADE_FLAG"]
+df = pd.DataFrame(columns=headings)
 for season in season_to_player_ids :
-    for player_id in season_to_player_ids[season]: 
+    for player_id in season_to_player_ids[season][:1]: 
         player_games_all = shot_url % (season, str(player_id))
         response = requests.get(player_games_all, headers={"USER-AGENT":u_a})
         shot_data = response.json()['resultSets'][0]['rowSet']
         for shot in shot_data:
-            print shot
+            print(shot[4])
+            dfTemp = pd.DataFrame([shot], columns=headings)
+            df = df.append(dfTemp)
+
+
         
-        
-        
+with open('shot_details_short.csv' ,'a') as out:
+  df.to_csv(out, header=True)
+
+
+         

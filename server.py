a11fa93cee647eb26c15bca8f148e0182e73b20e
Brandon Zhang <bwzhang@gmail.com>
2016-09-24 12:47:43 -0400

    Added flask server

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+import pandas as pd
+import json
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy import inspect
+
+def object_as_dict(obj):
+  return {c.key: getattr(obj, c.key)
+          for c in inspect(obj).mapper.column_attrs}
+
+engine = create_engine('sqlite:///nba.db')
+
+def run_query(query):
+  sql = text(query)
+  result = engine.execute(sql)
+  rows = []
+  for row in result:
+    d = {}
+    for column in row.keys():
+      d[column] = row[column]
+    rows.append(d)
+  return rows
+
+app = Flask(__name__)
+
+@app.route("/query/<query_str>")
+def query(query_str):
+    # Example query
+    # rows = run_query('select * from teamid_link where "index" > 5')
+    rows = run_query(query_str)
+    return json.dumps(rows)
+
+if __name__ == "__main__":
+    app.run()
